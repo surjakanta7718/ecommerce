9ed41b68af2499a5cd7ce5712ca505c6940e52d3
Surjakanta Das <surjakantadas.cse2023@nsec.ac.in>
2025-08-29 21:48:18 +0530

    unis

--- backend/index.js
+++ backend/index.js
@@ -8,21 +8,21 @@ const serverless = require("serverless-http");
 dotenv.config();
 const app = express();
 
-// const allowedOrigins = [
-//   "https://ecommerce-hazel-tau-48.vercel.app",
-//   "http://localhost:3000"
-// ];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
+const allowedOrigins = [
+  "https://ecommerce-hazel-tau-48.vercel.app",
+  "http://localhost:3000"
+];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend API is running 🚀", status: "ok" });
@@ -40,10 +40,10 @@ app.use("/api/products/:productId/reviews", require("./routes/review"));
 connectDB();
 
 // Local dev only
-// if (require.main === module) {
-//   const PORT = process.env.PORT || 5000;
-//   app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-//}
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+}
 
 // 👉 Export app (important for Vercel)
 //module.exports = app;
@@ -55,7 +55,7 @@ connectDB();
 // });
 
 
-module.exports = serverless(app);
+//module.exports = serverless(app);
 
 
 // const express = require("express");
@@ -75,5 +75,5 @@ module.exports = serverless(app);
 // app.use("/api/products", require("./routes/products"));
 // // add more routes here
 
-// module.exports = app;
+ module.exports = app;
  module.exports.handler = serverless(app);
